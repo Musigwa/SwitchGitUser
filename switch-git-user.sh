12fcf80217d67fa16627b5047674eb4d8d65bbdd
MUSIGWA Pacifique <pacifique.musigwa@gmail.com>
2024-04-03 20:28:36 +0200

    Setup the basic git user's name and email information

--- switch-git-user.sh
+++ switch-git-user.sh
@@ -2,8 +2,82 @@
 
 # This is the main function that runs the rest
 main() {
+    select_git_account
     add_ssh_keys_to_agent
     select_gpg_key
+}
+
+select_git_account() {
+    # Enable nocasematch
+    shopt -s nocasematch
+
+    # Define a list of hardcoded names and emails
+    declare -a accounts=("pacifique.musigwa@gmail.com" "p.musigwa@irembo.com")
+
+    # Default account's name and email 
+    declare name="MUSIGWA Pacifique"
+    declare email="${accounts[0]}"
+
+    indices=""
+    for ((i = 0; i < ${#accounts[@]}; i++)); do
+        indices+="$(($i + 1)), "
+        echo "$((${i} + 1)) ${name} - ${accounts[$i]}"
+    done
+
+    echo "$((${#accounts[@]} + 1))) Enter your own name and email"
+    
+    # Let's get the user's input from the command line
+    # while true; do
+    read -p "Enter the number of the account, or $((${#accounts[@]} + 1)) to enter your own. q(quit): " selection
+    case $selection in
+        1)
+            email=$(echo "${accounts[0]}")
+            break
+            ;;
+        2)
+            email=$(echo "${accounts[1]}")
+            break
+            ;;
+        $((${#accounts[@]} + 1)))
+            read -p "Enter the account full name (space separated): " name
+            read -p "Enter the account email: " email
+            ;;
+        q|quit)
+            exit 0
+            ;;
+        *)
+            echo "Invalid input. Please choose one of these: $indices"or" $((${#accounts[@]} + 1))."
+            ;;
+    esac
+    # done
+
+    while true; do
+        read -p "Set as (--global)? Otherwise, make sure you're in a git repo. (y/N): " global        
+        case $global in
+            y|yes)
+                global=--global
+                break
+                ;;
+            n|no)
+                global=
+                break
+                ;;
+            q|quit)
+                exit 0
+                ;;
+            *)
+                echo "Invalid input. Please enter y/yes, n/no. (Case insensitive)"
+                ;;
+        esac
+    done
+
+    # Set the user configurations as necessary
+    echo "Setting user configurations as: $name - $email"
+    git config $global user.name "${name}"
+    git config $global user.email "${email}"
+
+    # Disable nocasematch if you don't want it to affect other parts of your script
+    shopt -u nocasematch
 }
 
 add_ssh_keys_to_agent() {
